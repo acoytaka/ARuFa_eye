ea8f454b6c3e7655aabbf9025a8e644b85a75aaf
Takahashi_Yuki <46366459+acoytaka@users.noreply.github.com>
2022-07-31 00:10:49 +0900

    Update arufa_eye.py

--- arufa_eye.py
+++ arufa_eye.py
@@ -1,7 +1,7 @@
 import cv2
 import sys
 
-camera_id = 1
+camera_id = 0   # カメラIDは環境によって変わります。大体は0か1で動くはず
 delay = 1
 window_name = 'frame'
 
@@ -23,18 +23,18 @@ if not cap.isOpened():
 while True:
     ret, frame = cap.read()
     frame = cv2.resize(frame, dsize=None, fx=0.5,
-                       fy=0.5)   # 解像度が大きいとご検出がめっさ増える
+                       fy=0.5)   # 解像度が大きいと誤検出がめっさ増える
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray)
+    faces = face_cascade.detectMultiScale(gray) # 顔検出
 
     for x, y, w, h in faces:
-        face = frame[y: y + round(h/2), x: x + w]   # 鼻のご検出を防ぐ
+        face = frame[y: y + round(h/2), x: x + w]   # 鼻の誤検出を防ぐ
         face_gray = gray[y: y + round(h/2), x: x + w]
-        eyes = eye_cascade.detectMultiScale(face_gray)
+        eyes = eye_cascade.detectMultiScale(face_gray)  # 目の検出
 
-        # こういう書き方は良くない
+        # こういう書き方は良くない。目の位置と顔の大きさを把握していい感じに描画している。適当だから適宜調整してください。
         try:
             if eyes[0][0] < eyes[1][0]:
                 addw = (w - eyes[1][0] - eyes[0][0])/3
@@ -54,7 +54,7 @@ while True:
             pass
 
     cv2.imshow(window_name, frame)
-    if cv2.waitKey(delay) & 0xFF == ord('q'):
+    if cv2.waitKey(delay) & 0xFF == ord('q'):   # q押すと終了
         break
 
 cv2.destroyWindow(window_name)
